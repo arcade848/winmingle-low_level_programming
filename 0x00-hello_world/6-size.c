9ac5e5b708fd881a5375ca8be754a2e7d09d1381
arcade848 <jimohidris848@gmail.com>
2026-07-18 22:32:33 +0100

    a C program that prints the size of various types of data  on a machine

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*Author:Idris Jimoh
+*Program:Winmingle C Training
+*Description
+*/
+int main(void){
+printf("Size of a char: 1 byte(s)\n"
+       "Size of an int: 4 byte(s)\n"
+	   "Size of a long int: 8 byte(s)\n"
+	   "Size of a long long int: 8 byte(s)\n"
+	   "size of a float: 4 byte(s)\n"
+	   );
+return 0;
+}
