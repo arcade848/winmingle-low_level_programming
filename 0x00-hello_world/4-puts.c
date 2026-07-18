96928a6633ad7745b493528c69c20ca9df0b06ce
arcade848 <jimohidris848@gmail.com>
2026-07-18 22:23:50 +0100

    a C program that puts to print

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/* Author:Idris Jimoh
+*Program:Winmingle C Training
+*Description:This program uses puts function to print an output 
+*/
+int main(void) {
+puts("\"Programming is like building a multilingual puzzle\"\n");
+return 0;
+}
