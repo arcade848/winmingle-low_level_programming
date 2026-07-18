976fa2da3b1b9e4f7bb9a695d4f3a62ce1307314
arcade848 <jimohidris848@gmail.com>
2026-07-18 22:25:31 +0100

    a C program that is compiled with -Wall

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/*Author:Idris Jimoh
+*Program:Winmingle C Training
+*Description: This grogram is compiled without warnings using -wall
+*/
+int main(void) {
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return 0;
+}
