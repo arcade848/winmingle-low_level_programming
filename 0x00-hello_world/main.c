a03d25bf5d7ea1b9b13c1d787f541e386608312f
arcade848 <jimohidris848@gmail.com>
2026-07-18 22:02:40 +0100

    The main c file whose output is stored in another file

---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,11 @@
+/**
+*Author: Idris jimoh
+*Program: Winmingle Community C Training 
+*Description: This file uses a bash script to run through the precessor
+and saves it output in another c file named c
+*/
+#include <stdio.h>
+int main(void)  { 
+printf("Hello Winmingle \n");
+return 0;
+} 
