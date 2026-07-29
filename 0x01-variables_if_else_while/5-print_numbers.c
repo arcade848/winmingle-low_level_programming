8a38e88db40db9ef2b7ac905b21da139a9a971f2
arcade848 <jimohidris848@gmail.com>
2026-07-29 23:40:06 +0100

    adding a C file that prints all single digits of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/*Author:Idris Jimoh
+ * Program:Winmingle Community C Training
+ * Description: A program that prints all single digit of base 10
+ */
+int main(){
+//variable declaration
+   char b ='0';
+//loop and condition
+while(b<='9'){
+putchar(b);
+b++;
+}
+putchar('\n');
+return 0;
+}
