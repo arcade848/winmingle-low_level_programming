67e4566b13badbf27dab1934a8c3c120756629e2
arcade848 <jimohidris848@gmail.com>
2026-07-30 23:35:49 +0100

    adding a c file that prints lowercase letters in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/*Author:Jimoh Idris
+*Program:Winmingle Community c training
+*Description:Printind lowercase letters in reverse order
+*/
+int main(){
+//declaring variable
+char f='z';
+//loop and condition statement
+while(f>='a') {
+putchar(f);
+f--;
+
+} 
+putchar('\n');
+return 0;
+}
