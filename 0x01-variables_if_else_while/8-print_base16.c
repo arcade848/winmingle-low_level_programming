1cd5fe4e56bc9e9b0f2b40e532725eb1cf6033d3
arcade848 <jimohidris848@gmail.com>
2026-07-30 23:56:12 +0100

    adding a c file that prints hexadecimal numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+//declaring variable
+char h ='0';
+//loop and conditional statement
+while(h<='9'){
+putchar(h);
+//incremental
+h++;
+} 
+//declaring variable
+ h='a';
+while(h<='f'){
+putchar(h);
+h++;
+}
+putchar('\n');
+return 0;
+}
