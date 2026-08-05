b10092ac5fe5a8b96b9fbb2703dbf0cfd69c627c
arcade848 <jimohidris848@gmail.com>
2026-08-05 22:13:13 +0100

    header file for the directory

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,8 @@
+/*Author:Idris jimoh
+ *Program:Winmingle Community C Training
+ *Description:defining the _putchar func.
+ *defining the void print_... func.
+ */
+ int _putchar(char c);
+ void print_alphabet(void);
+ void print_alphabet_x10(void);
