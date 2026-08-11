6689876fbc7ccd7ac93803ca2f820ddb6b24f08a
arcade848 <jimohidris848@gmail.com>
2026-08-11 12:35:45 +0100

    a cfile that prints all time of the day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/*Author:Idris Jimoh
+Program:Winmingle Community C training
+Description:jack_bauer prints every minute of the day
+*/
+void jack_bauer(void) {
+int hours;
+int minutes;
+for(hours = 0; hours <= 23 ; hours++) {
+for(minutes = 0; minutes <= 59 ; minutes++) {
+_putchar(( hours / 10) + '0');
+_putchar(( hours % 10) + '0');
+_putchar(':');
+_putchar(( minutes /10) + '0');
+_putchar(( minutes % 10) + '0');
+_putchar('\n');                                                                                  
+}
+}
+}
