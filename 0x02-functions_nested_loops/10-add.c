becf1e8651518e0abd324a0572e0c43229898f63
arcade848 <jimohidris848@gmail.com>
2026-08-30 00:26:36 +0100

    a function that adds two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/*Author:Idris Jimoh
+*Program:Winmingle Community C Training
+*Description:A C file that adds two numbers
+*return- returns the addition of two integers a and b
+*/
+int add(int a, int b) { 
+return ( a + b);
+}
