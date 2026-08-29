cf8a15d24ac0c0235ca90171b6864729d3d8d4bc
arcade848 <jimohidris848@gmail.com>
2026-08-30 00:51:07 +0100

    a program that computes and prints the sum of all the multiples of 3 or 5 below 1024

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/*Author:Idris Jimoh
+*Program:Winmingle Community C Training
+*Description:
+*/
+int main( ) {
+    int i;
+    int sum = 0;
+    for(i = 1; i < 1024; i++) {
+       if( i % 3 == 0 || i % 5 == 0 ) {
+          sum += i;
+
+        } 
+
+     }   
+     printf("%d\n",sum);
+     return 0;
+} 
