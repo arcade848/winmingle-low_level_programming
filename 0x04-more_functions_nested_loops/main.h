9ae07c81272199c0ee062650065283c5e1164aed
arcade848 <jimohidris848@gmail.com>
2026-09-05 22:55:26 +0100

    header file for the directory

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,7 +7,12 @@
  * On error,-1 is returned, and error is set appropriately.
  */
 int _isupper(int c);
-int _putchar(char c){
-	return (write(1, &c, 1));
-}
 int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+int _putchar(char c);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
