048182a949bc21444b875880c0b09391b4d190b7
arcade848 <jimohidris848@gmail.com>
2026-09-05 22:27:39 +0100

    a file that defines a func that multiplies two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,9 @@
+/**
+*Author:Idris Jimoh
+*Program:Winmingle Community C Training
+*Description: A file that returns the product of two integers
+**/
+int mul(int a, int b);// prototype declaration
+int mul(int a, int b){// function definition
+   return a * b; // returns multiplication of a and b
+}
