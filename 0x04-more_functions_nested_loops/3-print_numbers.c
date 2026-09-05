46eb41700ab4c1b4251832162714bcdf71350eb5
arcade848 <jimohidris848@gmail.com>
2026-09-05 22:34:29 +0100

    a func that prints nhmbers 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * Author:Idris Jimoh
+ * Program:Winmingle Community C Training
+ * Description:a file that prints integers
+ **/
+
+void print_numbers(void){//function definition
+ char c ; //variable declaration
+ for ( c = '0'; c <='9'; c++){//loop intialization
+   _putchar(c);//printing to the screen
+ }
+ _putchar('\n');
+}
